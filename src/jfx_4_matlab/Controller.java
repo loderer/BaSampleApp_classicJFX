24b7cd818855e63201562f17724f55cd2b5a18ce
Rudi Loderer <loderer@hm.edu>
2018-06-27 15:48:39 +0200

    Enhance documentation.

--- src/jfx_4_matlab/Controller.java
+++ src/jfx_4_matlab/Controller.java
@@ -4,13 +4,19 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-
+/**
+ * Controller for the sample scene.
+ */
 public class Controller {
+    // ui-controls injected by the javaFX-framework.
     @FXML
     Label lbl;
     @FXML
     Button btn;
 
+    /**
+     * Handle click on btn.
+     */
     public void clickButton() {
         lbl.setText("heureka");
     }
